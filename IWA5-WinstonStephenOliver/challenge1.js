77f371927e7f405805d56f35730dfb8e5e84f063
Woliver4 <woliver4@gmail.com>
2023-04-03 13:55:39 +0200

    first commit

---
+++ IWA5-WinstonStephenOliver/challenge1.js
@@ -0,0 +1,37 @@
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = '0'
+
+let customers = '1'
+let currency = 'R'
+let location = RSA
+let shipping = 0
+ 
+
+if (location = RSA) { shipping === 400 && currency === 'R' }
+
+
+
+if (location = NAM) { shipping === 600 && currency === '$'}
+
+
+else {shipping === 800 && currency === '$' }
+
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * 'NONE_SELECTED'
+let batteries = 35 * 2
+let pens = 5 * 'NONE_SELECTED' 
+
+if (shoes + batteries + pens + shirts + toys >= 1000 && location === RSA  || (location = NAM) 60 {
+	if (location = NAM && customers < 2) {0
+			if (location = RSA)
+		    shipping = 0 || calcShipping
+		}
+	}
+
+
+if(shipping = 0) && (customers !== 1) { console.log(WARNING) }
+
+location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping + toys)
+
